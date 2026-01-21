9b554f9381a067d55f6ee91ca6083717b13002df
uno/hurtki <asboba2101@gmail.com>
2026-01-21 13:04:55 +0200

    add: implementation of AddUserData and tests

--- api/internal/repo/github_user_data/storage.go
+++ api/internal/repo/github_user_data/storage.go
@@ -45,6 +45,8 @@ func (r *GithubDataPsgrRepo) UpdateUserData(userData domain.GithubUserData) (err
 		if err != nil {
 			err := tx.Rollback()
 			r.logger.Error("error, when rolling back transaction", "err", err, "source", fn)
+		} else {
+			err = tx.Commit()
 		}
 	}()
 
@@ -169,23 +171,79 @@ func (r *GithubDataPsgrRepo) UpdateUserData(userData domain.GithubUserData) (err
 	return toRepoError(err)
 }
 
-// WIP
 func (r *GithubDataPsgrRepo) AddUserData(userData domain.GithubUserData) error {
-	res, err := r.db.Exec(`
-	insert into users (username, name, company, location, bio, publi_repos_count, followers_count, following_count, fetched_at)
+	fn := "internal.repo.github_user_data.GithubDataPsgrRepo.AddUserData"
+	tx, err := r.db.Begin()
+	if err != nil {
+		r.logger.Error("can't start transaction", "source", fn, "err", err)
+		return repo.ErrRepoInternal{
+			Note: err.Error(),
+		}
+	}
+
+	defer func() {
+		// if some error is being returned we will rollback transaction
+		if err != nil {
+			err := tx.Rollback()
+			r.logger.Error("error, when rolling back transaction", "err", err, "source", fn)
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	// trying to insert user data
+	_, err = tx.Exec(`
+	insert into users (username, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at)
 	values ($1, $2, $3, $4, $5, $6, $7, $8, $9);
 	`, userData.Username, userData.Name, userData.Company, userData.Location, userData.Bio, userData.PublicRepos, userData.Followers, userData.Following, userData.FetchedAt)
 
+	// if there is an error, return it converted to repo level error
+	// including error with if userData with same username already exists, it will be converted to ErrConflictValue
 	if err != nil {
 		return toRepoError(err)
 	}
 
-	count, _ := res.RowsAffected()
-	if count < 1 {
-		return repo.ErrNothingChanged
+	// before inserting into repositories, check if we are inserting at least one
+	// if not skip
+	if len(userData.Repositories) < 1 {
+		return nil
 	}
 
-	return nil
+	var (
+		posParams []string
+		args      []any
+	)
+
+	// building query for inserting repos
+	i := 1
+	for _, repo := range userData.Repositories {
+		tempPosArgs := []string{}
+		for j := i; j < i+8; j++ {
+			tempPosArgs = append(tempPosArgs, fmt.Sprintf("$%d", j))
+		}
+		posParams = append(posParams, fmt.Sprintf("(%s)", strings.Join(tempPosArgs, ", ")))
+		args = append(args,
+			repo.ID,
+			repo.OwnerUsername,
+			repo.PushedAt,
+			repo.UpdatedAt,
+			repo.Language,
+			repo.StarsCount,
+			repo.Fork,
+			repo.ForksCount,
+		)
+		i++
+	}
+
+	// inserting positional arguments into query
+	query := fmt.Sprintf(`
+	insert into repositories (github_id, owner_username, pushed_at, updated_at, language, stars_count, is_fork, forks_count)
+	values (%s);
+	`, strings.Join(posParams, ", "))
+
+	_, err = tx.Exec(query, args...)
+
+	return toRepoError(err)
 }
 
 // WIP
--- api/internal/repo/github_user_data/storage_test.go
+++ api/internal/repo/github_user_data/storage_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/hurtki/github-banners/api/internal/domain"
 	"github.com/hurtki/github-banners/api/internal/logger"
+	repoerr "github.com/hurtki/github-banners/api/internal/repo"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,14 +20,87 @@ func (m LoggerMock) Warn(a string, b ...any)     {}
 func (m LoggerMock) Error(a string, b ...any)    {}
 func (m LoggerMock) With(a ...any) logger.Logger { return m }
 
-func TestStorageUpdateUserDataSuccess(t *testing.T) {
+// helper for testing to create sql mock, create repoistory with it
+func getMockAndRepo() (sqlmock.Sqlmock, *GithubDataPsgrRepo) {
 	db, mock, _ := sqlmock.New(
 		sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual),
 	)
 	logger := LoggerMock{}
 
 	repo := NewGithubDataPsgrRepo(db, logger)
+	return mock, repo
+}
 
+func TestStorageAddUserDataSucess(t *testing.T) {
+	mock, repo := getMockAndRepo()
+	githubRepo := domain.GithubRepository{ID: 123, OwnerUsername: "alex"}
+	userData := domain.GithubUserData{
+		Username:     "alex",
+		FetchedAt:    time.Now(),
+		Repositories: []domain.GithubRepository{githubRepo},
+	}
+
+	mock.ExpectBegin()
+
+	mock.ExpectExec(`
+	insert into users (username, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at)
+	values ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+	`).WithArgs(userData.Username, userData.Name, userData.Company, userData.Location, userData.Bio, userData.PublicRepos, userData.Followers, userData.Following, userData.FetchedAt).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectExec(`
+	insert into repositories (github_id, owner_username, pushed_at, updated_at, language, stars_count, is_fork, forks_count)
+	values (($1, $2, $3, $4, $5, $6, $7, $8));
+	`).WithArgs(githubRepo.ID, githubRepo.OwnerUsername, githubRepo.PushedAt, githubRepo.UpdatedAt, githubRepo.Language, githubRepo.StarsCount, githubRepo.Fork, githubRepo.ForksCount).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectCommit()
+
+	err := repo.AddUserData(userData)
+	require.NoError(t, err)
+}
+
+func TestStorageAddUserDataAlreadyExistsErrorCheck(t *testing.T) {
+	mock, repo := getMockAndRepo()
+	githubRepo := domain.GithubRepository{ID: 123, OwnerUsername: "alex"}
+	userData := domain.GithubUserData{
+		Username:     "alex",
+		FetchedAt:    time.Now(),
+		Repositories: []domain.GithubRepository{githubRepo},
+	}
+
+	mock.ExpectBegin()
+
+	mock.ExpectExec(`
+	insert into users (username, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at)
+	values ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+	`).WithArgs(userData.Username, userData.Name, userData.Company, userData.Location, userData.Bio, userData.PublicRepos, userData.Followers, userData.Following, userData.FetchedAt).WillReturnError(&pgconn.PgError{Code: "23505"})
+
+	mock.ExpectRollback()
+
+	require.Equal(t, repo.AddUserData(userData), &repoerr.ErrConflictValue{})
+}
+
+func TestStorageAddUserDataNoRepositoriesSuccess(t *testing.T) {
+	mock, repo := getMockAndRepo()
+	userData := domain.GithubUserData{
+		Username:     "alex",
+		FetchedAt:    time.Now(),
+		Repositories: make([]domain.GithubRepository, 0),
+	}
+
+	mock.ExpectBegin()
+
+	mock.ExpectExec(`
+	insert into users (username, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at)
+	values ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+	`).WithArgs(userData.Username, userData.Name, userData.Company, userData.Location, userData.Bio, userData.PublicRepos, userData.Followers, userData.Following, userData.FetchedAt).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectCommit()
+
+	require.Equal(t, repo.AddUserData(userData), nil)
+}
+
+func TestStorageUpdateUserDataSuccess(t *testing.T) {
+	mock, repo := getMockAndRepo()
 	githubRepo := domain.GithubRepository{ID: 123, OwnerUsername: "alex"}
 	userData := domain.GithubUserData{
 		Username:     "alex",
