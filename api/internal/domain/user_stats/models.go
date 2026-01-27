57c532d494ea40777b03ea9f3e2a818dea1b7734
uno/hurtki <asboba2101@gmail.com>
2026-01-27 21:00:45 +0200

    refactor: move RefreshAll logic to domain, rm: old service, fix: repo interface

--- api/internal/domain/user_stats/models.go
+++ api/internal/domain/user_stats/models.go
@@ -6,16 +6,10 @@ import (
 	"github.com/hurtki/github-banners/api/internal/domain"
 )
 
-const (
-	SoftTTL = 10 * time.Minute
-	HardTTL = 24 * time.Hour
-)
-
 type UserStatsService struct {
-	repo    GithubStatsRepository
+	repo    GithubUserDataRepository
 	fetcher UserDataFetcher
 	cache   Cache
-	cfg     WorkerConfig
 }
 
 type CachedStats struct {
@@ -27,10 +21,4 @@ type WorkerConfig struct {
 	BatchSize   int
 	Concurrency int
 	CacheTTL    time.Duration
-}
-
-// WorkerResult holds the result of processing a user
-type WorkerResult struct {
-	Username string
-	Error    error
 }
