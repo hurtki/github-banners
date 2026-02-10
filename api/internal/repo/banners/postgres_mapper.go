aa521e00f5918815da419faf108efd23591e43fd
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-02-10 23:59:17 +0530

    refactor: added logger finally :)

--- api/internal/repo/banners/postgres_mapper.go
+++ api/internal/repo/banners/postgres_mapper.go
@@ -5,17 +5,25 @@ import (
 	repoerr "github.com/hurtki/github-banners/api/internal/repo"
 )
 
-func bannerTypeToDB(bt domain.BannerType) (string, error) {
+func (r *PostgresRepo) bannerTypeToDB(bt domain.BannerType) (string, error) {
+	fn := "internal.repo.banners.PostgresRepo.bannerTypeToDB"
 	v, ok := domain.BannerTypesBackward[bt]
 	if !ok {
+		if r.logger != nil {
+			r.logger.Error("unknown banner type", "source", fn, "banner_type", bt)
+		}
 		return "", repoerr.ErrRepoInternal{Note: "unknown banner type"}
 	}
 	return v, nil
 }
 
-func bannerTypeFromDB(v string) (domain.BannerType, error) {
+func (r *PostgresRepo) bannerTypeFromDB(v string) (domain.BannerType, error) {
+	fn := "internal.repo.banners.PostgresRepo.bannerTypeFromDB"
 	bt, ok := domain.BannerTypes[v]
 	if !ok {
+		if r.logger != nil {
+			r.logger.Error("unknown banner type", "source", fn, "banner_type", v)
+		}
 		return 0, repoerr.ErrRepoInternal{Note: "unknown banner type"}
 	}
 	return bt, nil
