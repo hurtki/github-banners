34bf3360db6b8fb4be74f85c90716735d4fa072c
uno/hurtki <asboba2101@gmail.com>
2026-02-11 17:50:41 +0200

    fix: error response function + add: better loging and code style

--- api/internal/handlers/error_response.go
+++ api/internal/handlers/error_response.go
@@ -8,22 +8,24 @@ import (
 // errror is used to write error in json
 // if error, when marshaling appears, handles and logs it
 func (h *BannersHandler) error(rw http.ResponseWriter, statusCode int, message string) {
-	resJson := make(map[string]string)
-	resJson["error"] = message
-	res, err := json.Marshal(resJson)
+	fn := "internal.handlers.BannersHandler.error"
+	rw.Header().Set("Content-Type", "application/json")
+	res, err := json.Marshal(map[string]string{"error": message})
+
 	if err != nil {
-		h.logger.Error("canl't marshal error to response")
+		h.logger.Error("can't marshal error response", "err", err, "source", fn)
 		rw.WriteHeader(http.StatusInternalServerError)
 		_, err := rw.Write([]byte("{\"error\": \"server error occured\"}"))
 		if err != nil {
-			h.logger.Warn("can't write error response")
-			return
+			h.logger.Warn("can't write error response", "err", err, "source", fn)
 		}
+		return
 	}
+
 	rw.WriteHeader(statusCode)
 	_, err = rw.Write(res)
 	if err != nil {
-		h.logger.Warn("can't write error response")
+		h.logger.Warn("can't write error response", "err", err, "source", fn)
 		return
 	}
 }
