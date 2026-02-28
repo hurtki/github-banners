691f06ca0655bc47fe5238fa997a10787f13e01b
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-02-28 18:33:50 +0530

    refactor(domain): align BannerInfo with Kafka DTO and API spec
    Added UrlPath, converted BannerType to an enum, and removed raw GitHub profile structs since the renderer only requires aggregated stats.

--- renderer/internal/domain/types.go
+++ renderer/internal/domain/types.go
@@ -5,52 +5,59 @@ import (
 	"time"
 )
 
+type BannerType string
+
+const (
+	BannerTypeDefault BannerType = "default"
+	BannerTypeDark    BannerType = "dark"
+)
+
 type BannerInfo struct {
-    Username    string
-    BannerType  string
-    Profile     GithubUserData
-    Stats       GithubUserStats
+	Username   string
+	BannerType BannerType
+	URLPath    string
+	Stats      GithubUserStats
 }
 
 type RenderedBanner struct {
-    Filename    string
-    Data        []byte
+	Filename string
+	Data     []byte
 }
 
 type GithubUserData struct {
-    Username    string
-    Name        *string
-    Company     *string
-    Location    *string
-    Bio         *string
-    PublicRepos int
-    Followers   int
-    Following   int
-    Repositories []GithubRepository
-    FetchedAt   time.Time
+	Username     string
+	Name         *string
+	Company      *string
+	Location     *string
+	Bio          *string
+	PublicRepos  int
+	Followers    int
+	Following    int
+	Repositories []GithubRepository
+	FetchedAt    time.Time
 }
 
 type GithubUserStats struct {
-    TotalRepos    int
-    OriginalRepos int
-    ForkedRepos   int
-    TotalStars    int
-    TotalForks    int
-    Languages     map[string]int
-    FetchedAt     time.Time
+	TotalRepos    int
+	OriginalRepos int
+	ForkedRepos   int
+	TotalStars    int
+	TotalForks    int
+	Languages     map[string]int
+	FetchedAt     time.Time
 }
 
 type GithubRepository struct {
-    ID            int64
-    OwnerUsername string
-    PushedAt      *time.Time
-    UpdatedAt     *time.Time
-    Language      *string
-    StarsCount    int
-    Fork          int
-    ForksCount    int
+	ID            int64
+	OwnerUsername string
+	PushedAt      *time.Time
+	UpdatedAt     *time.Time
+	Language      *string
+	StarsCount    int
+	Fork          int
+	ForksCount    int
 }
 
 type BannerStorage interface {
-    Save(ctx context.Context, banner RenderedBanner) error
+	Save(ctx context.Context, banner RenderedBanner) error
 }
