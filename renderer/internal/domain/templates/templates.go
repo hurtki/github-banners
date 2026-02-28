ddaa5c0bb2eb69084f915e646bb047653a0da8de
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-02-28 18:34:43 +0530

    feat(templates): embed svg designs in feature package
    Moved template embedding logic directly into the render package to support the modular package-by-feature architecture.

--- renderer/internal/domain/templates/templates.go
+++ renderer/internal/domain/templates/templates.go
@@ -5,8 +5,9 @@ import (
 	"html/template"
 )
 
+//go:embed *.svg
 var bannerFiles embed.FS
 
-func Load()(*template.Template, error) {
+func Load() (*template.Template, error) {
 	return template.ParseFS(bannerFiles, "*.svg")
 }
