f3420b5b24ea949cc7a0af450da606fef2123001
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-03-04 23:42:08 +0530

    fix(renderer): fix the json tags typography

--- api/internal/infrastructure/renderer/dto.go
+++ api/internal/infrastructure/renderer/dto.go
@@ -1,6 +1,10 @@
 package renderer
 
-import "github.com/hurtki/github-banners/api/internal/domain"
+import (
+	"time"
+
+	"github.com/hurtki/github-banners/api/internal/domain"
+)
 
 // GithubUserBannerInfo is a struct, that describes data that is used to render banner
 type GithubUserBannerInfo struct {
@@ -19,24 +23,32 @@ func FromDomainBannerInfo(bi domain.BannerInfo) GithubUserBannerInfo {
 
 func (i GithubUserBannerInfo) ToBannerPreviewRequest() bannerPreviewRequest {
 	return bannerPreviewRequest{
-		Username:      i.Username,
-		BannerType:    i.BannerType,
-		TotalRepos:    i.Stats.TotalRepos,
-		OriginalRepos: i.Stats.OriginalRepos,
-		ForkedRepos:   i.Stats.ForkedRepos,
-		TotalStars:    i.Stats.TotalStars,
-		TotalForks:    i.Stats.TotalForks,
-		Languages:     i.Stats.Languages,
+		Username:   i.Username,
+		BannerType: i.BannerType,
+		Stats: bannerPreviewStats{
+			TotalRepos:    i.Stats.TotalRepos,
+			OriginalRepos: i.Stats.OriginalRepos,
+			ForkedRepos:   i.Stats.ForkedRepos,
+			TotalStars:    i.Stats.TotalStars,
+			TotalForks:    i.Stats.TotalForks,
+			Languages:     i.Stats.Languages,
+		},
+		FetchedAt: i.Stats.FetchedAt,
 	}
 }
 
 type bannerPreviewRequest struct {
-	Username      string         `json:"username"`
-	BannerType    string         `json:"banner_type"`
-	TotalRepos    int            `json:"total_repos"`
-	OriginalRepos int            `json:"original_repos"`
-	ForkedRepos   int            `json:"forked_repos"`
-	TotalStars    int            `json:"total_stars"`
-	TotalForks    int            `json:"total_forks"`
+	Username   string             `json:"username"`
+	BannerType string             `json:"banner_type"`
+	Stats      bannerPreviewStats `json:"stats"`
+	FetchedAt  time.Time          `json:"fetched_at"`
+}
+
+type bannerPreviewStats struct {
+	TotalRepos    int            `json:"totalRepos"`
+	OriginalRepos int            `json:"originalRepos"`
+	ForkedRepos   int            `json:"forkedRepos"`
+	TotalStars    int            `json:"totalStars"`
+	TotalForks    int            `json:"totalForks"`
 	Languages     map[string]int `json:"languages"`
 }
