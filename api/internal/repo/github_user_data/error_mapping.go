900c9925e076fae09b713d70e96dea2c5d869d1b
uno/hurtki <asboba2101@gmail.com>
2026-03-08 15:45:37 +0200

    refactor: toRepoErr to handleErr to log important errors with context

--- api/internal/repo/github_user_data/error_mapping.go
+++ api/internal/repo/github_user_data/error_mapping.go
@@ -8,7 +8,9 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
-func toRepoError(err error) error {
+// handleError converts error to repo errors and logs about
+// important ones using fn as "source" field in logger to understand context of error
+func (r *GithubDataPsgrRepo) handleError(err error, fn string) error {
 	if err == nil {
 		return nil
 	}
@@ -22,12 +24,16 @@ func toRepoError(err error) error {
 			return &repoerr.ErrConflictValue{Field: pgErr.ConstraintName}
 		case "23502":
 			return &repoerr.ErrEmptyField{Field: pgErr.ColumnName}
+		// 42601 syntax error code for postgres
 		case "42601":
+			r.logger.Error("syntax error from postgres", "err", err, "source", fn)
 			return &repoerr.ErrRepoInternal{Note: pgErr.Hint}
 		default:
+			r.logger.Error("unhandled PgError", "err", err, "source", fn)
 			return &repoerr.ErrRepoInternal{Note: pgErr.Message}
 		}
 	}
 
+	r.logger.Error("unhandled error from db", "err", err, "source", fn)
 	return &repoerr.ErrRepoInternal{Note: err.Error()}
 }
