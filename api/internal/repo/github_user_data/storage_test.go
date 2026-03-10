139f3c20149a8ee8cef72ec6436338b2e1ead65e
uno/hurtki <asboba2101@gmail.com>
2026-03-11 00:02:34 +0200

    fix: tests for new github data repo

--- api/internal/repo/github_user_data/storage_test.go
+++ api/internal/repo/github_user_data/storage_test.go
@@ -2,6 +2,7 @@ package github_data_repo
 
 import (
 	"context"
+	"strings"
 	"testing"
 	"time"
 
@@ -48,9 +49,10 @@ func TestSaveUserDataSucess(t *testing.T) {
 	mock.ExpectBegin()
 
 	mock.ExpectExec(`
-	insert into users (username, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at)
-	values ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-	on conflict (username) do update set
+	insert into github_data.users (username, username_normalized, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at)
+	values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+	on conflict (username_normalized) do update set
+		username = EXCLUDED.username,
 		name = EXCLUDED.name,
 		company = EXCLUDED.company,
 		location = EXCLUDED.location,
@@ -59,24 +61,24 @@ func TestSaveUserDataSucess(t *testing.T) {
 		followers_count = EXCLUDED.followers_count,
 		following_count = EXCLUDED.following_count,
 		fetched_at = EXCLUDED.fetched_at;
-	`).WithArgs(userData.Username, userData.Name, userData.Company, userData.Location, userData.Bio, userData.PublicRepos, userData.Followers, userData.Following, userData.FetchedAt).WillReturnResult(sqlmock.NewResult(1, 1))
+	`).WithArgs(userData.Username, strings.ToLower(userData.Username), userData.Name, userData.Company, userData.Location, userData.Bio, userData.PublicRepos, userData.Followers, userData.Following, userData.FetchedAt).WillReturnResult(sqlmock.NewResult(1, 1))
 
 	mock.ExpectExec(`
-	insert into repositories (github_id, owner_username, pushed_at, updated_at, language, stars_count, is_fork, forks_count)
+	insert into github_data.repositories (github_id, owner_username_normalized, pushed_at, updated_at, language, stars_count, is_fork, forks_count)
 	values ($1, $2, $3, $4, $5, $6, $7, $8), ($9, $10, $11, $12, $13, $14, $15, $16)
 	on conflict (github_id) do update set
-		owner_username = excluded.owner_username,
+		owner_username_normalized = excluded.owner_username_normalized,
 		pushed_at      = excluded.pushed_at,
 		updated_at     = excluded.updated_at,
 		language       = excluded.language,
 		stars_count    = excluded.stars_count,
 		is_fork        = excluded.is_fork,
 		forks_count    = excluded.forks_count;
-	`).WithArgs(githubRepo1.ID, githubRepo1.OwnerUsername, githubRepo1.PushedAt, githubRepo1.UpdatedAt, githubRepo1.Language, githubRepo1.StarsCount, githubRepo1.Fork, githubRepo1.ForksCount, githubRepo2.ID, githubRepo2.OwnerUsername, githubRepo2.PushedAt, githubRepo2.UpdatedAt, githubRepo2.Language, githubRepo2.StarsCount, githubRepo2.Fork, githubRepo2.ForksCount).WillReturnResult(sqlmock.NewResult(1, 1))
+	`).WithArgs(githubRepo1.ID, strings.ToLower(githubRepo1.OwnerUsername), githubRepo1.PushedAt, githubRepo1.UpdatedAt, githubRepo1.Language, githubRepo1.StarsCount, githubRepo1.Fork, githubRepo1.ForksCount, githubRepo2.ID, githubRepo2.OwnerUsername, githubRepo2.PushedAt, githubRepo2.UpdatedAt, githubRepo2.Language, githubRepo2.StarsCount, githubRepo2.Fork, githubRepo2.ForksCount).WillReturnResult(sqlmock.NewResult(1, 1))
 
 	mock.ExpectExec(`
-		delete from repositories r
-		where r.owner_username = $1
+		delete from github_data.repositories r
+		where r.owner_username_normalized = lower($1)
 		and not exists (
 			select 1
 			from (values ($2::bigint), ($3::bigint)) as v(github_id)
@@ -101,9 +103,10 @@ func TestSaveUserDataSucessNoRepos(t *testing.T) {
 	mock.ExpectBegin()
 
 	mock.ExpectExec(`
-	insert into users (username, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at)
-	values ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-	on conflict (username) do update set
+	insert into github_data.users (username, username_normalized, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at)
+	values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+	on conflict (username_normalized) do update set
+		username = EXCLUDED.username,
 		name = EXCLUDED.name,
 		company = EXCLUDED.company,
 		location = EXCLUDED.location,
@@ -112,11 +115,11 @@ func TestSaveUserDataSucessNoRepos(t *testing.T) {
 		followers_count = EXCLUDED.followers_count,
 		following_count = EXCLUDED.following_count,
 		fetched_at = EXCLUDED.fetched_at;
-	`).WithArgs(userData.Username, userData.Name, userData.Company, userData.Location, userData.Bio, userData.PublicRepos, userData.Followers, userData.Following, userData.FetchedAt).WillReturnResult(sqlmock.NewResult(1, 1))
+	`).WithArgs(userData.Username, strings.ToLower(userData.Username), userData.Name, userData.Company, userData.Location, userData.Bio, userData.PublicRepos, userData.Followers, userData.Following, userData.FetchedAt).WillReturnResult(sqlmock.NewResult(1, 1))
 
 	mock.ExpectExec(`
-		delete from repositories
-		where owner_username = $1;
+		delete from github_data.repositories
+		where owner_username_normalized = lower($1);
 	`).WithArgs(userData.Username).WillReturnResult(sqlmock.NewResult(1, 1))
 
 	mock.ExpectCommit()
@@ -134,7 +137,7 @@ func TestGetAllUsernamesSuccess(t *testing.T) {
 		usernameRows.AddRow(un)
 	}
 	mock.ExpectQuery(`
-	select username from users;
+	select username from github_data.users;
 	`).WillReturnRows(usernameRows)
 
 	resUsernames, err := repo.GetAllUsernames(context.TODO())
@@ -147,7 +150,7 @@ func TestGetAllUsernamesNoUsernames(t *testing.T) {
 	mock, repo := getMockAndRepo(t)
 
 	mock.ExpectQuery(`
-	select username from users;
+	select username from github_data.users;
 	`).WillReturnRows(sqlmock.NewRows([]string{"username"}))
 
 	resUsernames, err := repo.GetAllUsernames(context.TODO())
@@ -159,16 +162,19 @@ func TestGetAllUsernamesNoUsernames(t *testing.T) {
 func TestGetUserDataSuccess(t *testing.T) {
 	mock, repo := getMockAndRepo(t)
 
-	userData := domain.GithubUserData{Username: "Olivia"}
+	userData := domain.GithubUserData{Username: "OliVia"}
 	repo1 := domain.GithubRepository{ID: 123, OwnerUsername: userData.Username}
 	repo2 := domain.GithubRepository{ID: 3454, OwnerUsername: userData.Username}
 	userData.Repositories = []domain.GithubRepository{repo1, repo2}
+
 	userColumns := []string{"username", "name", "company", "location", "bio", "public_repos_count", "followers_count", "following_count", "fetched_at"}
-	githubRepoColumns := []string{"github_id", "owner_username", "pushed_at", "updated_at", "language", "stars_count", "is_fork", "forks_count"}
+
+	githubRepoColumns := []string{"github_id", "pushed_at", "updated_at", "language", "stars_count", "is_fork", "forks_count"}
 
 	githubReposRows := sqlmock.NewRows(githubRepoColumns)
+
 	for _, githubRepo := range userData.Repositories {
-		githubReposRows.AddRow(githubRepo.ID, githubRepo.OwnerUsername, githubRepo.PushedAt, githubRepo.UpdatedAt, githubRepo.Language, githubRepo.StarsCount, githubRepo.Fork, githubRepo.ForksCount)
+		githubReposRows.AddRow(githubRepo.ID, githubRepo.PushedAt, githubRepo.UpdatedAt, githubRepo.Language, githubRepo.StarsCount, githubRepo.Fork, githubRepo.ForksCount)
 	}
 
 	userRows := sqlmock.NewRows(userColumns)
@@ -176,13 +182,13 @@ func TestGetUserDataSuccess(t *testing.T) {
 	mock.ExpectBegin()
 
 	mock.ExpectQuery(`
-	select username, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at from users
-	where username = $1;
+	select username, name, company, location, bio, public_repos_count, followers_count, following_count, fetched_at from github_data.users
+	where username_normalized = lower($1);
 	`).WithArgs(userData.Username).WillReturnRows(userRows)
 
 	mock.ExpectQuery(`
-	select github_id, owner_username, pushed_at, updated_at, language, stars_count, is_fork, forks_count from repositories
-	where owner_username = $1;
+	select github_id, pushed_at, updated_at, language, stars_count, is_fork, forks_count from github_data.repositories
+	where owner_username_normalized = lower($1);
 	`).WithArgs(userData.Username).WillReturnRows(githubReposRows)
 	mock.ExpectCommit()
 
